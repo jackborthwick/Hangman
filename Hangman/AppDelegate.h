ecf50cbc711f50c4dcf6ffcec16db1a27e6df594
Jack Borthwick <jackmckeanborthwick@gmail.com>
2015-06-04 13:26:19 -0400

    Initial Commit

---
+++ Hangman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hangman
+//
+//  Created by Jack Borthwick on 6/4/15.
+//  Copyright (c) 2015 Jack Borthwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
